bf02b05a1d27aa2b8063346de919402cd07faceb
TianaYan <tianchen.yan2000@gmail.com>
2023-11-14 04:21:16 +0800

    feat: fix bug

--- test/Sample-Test1/test.cpp
+++ test/Sample-Test1/test.cpp
@@ -1,5 +1,4 @@
 #include "pch.h"
-//#include <gtest/gtest.h>
 #include <iostream>
 #include <vector>
 #include <cstdlib>
@@ -8,7 +7,6 @@
 #include <sstream>
 #include <cmath>
 #include<typeinfo>
-using namespace std;
 
 // Define simulation parameters
 const int width = 256;                // Width of the grid
@@ -21,6 +19,7 @@ double threshold = 0.1;
 const double dt = 0.06;               // Time step
 const int numIterations = 10000;
 const int outputInterval = 100;      // Output every 1000 iterations
+double laplaceU = 0.0, laplaceV = 0.0, dU = 0.0, dV = 1.0;
 
 // Initialize grid and constants
 std::vector<std::vector<double>> u(width, std::vector<double>(height, 1.0));
@@ -30,46 +29,12 @@ std::vector<std::vector<double>> v(width, std::vector<double>(height, 0.0));
 void init() {
 	std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-	for (int x = 0; x < width; ++x) {
+	for (int x = 0; x < width; ++x)
 		for (int y = 0; y < height; ++y) {
-			u[x][y] = 1.0;
-			if (x >= 100 && x <= 200 && y >= 100 && y <= 150) {
-				v[x][y] = (std::rand() % 100 - 0) / 100.0;  // Random ~ U[0.0,1.0)
-			}
-			else {
-				v[x][y] = 0.0;
-			}
+			u[x][y] = 0.0;
+			v[x][y] = 0.0;
 		}
-	}
 }
-
-// Function to write the u array to a VTK file
-void writeVTKFile(int iteration) {
-	std::stringstream ss;
-	ss << "output_" << iteration << ".vtk";
-
-	std::ofstream vtkFile(ss.str());
-	vtkFile << "# vtk DataFile Version 3.0\n";
-	vtkFile << "Gray-Scott Simulation Output\n";
-	vtkFile << "ASCII\n";
-	vtkFile << "DATASET STRUCTURED_POINTS\n";
-	vtkFile << "DIMENSIONS " << width << " " << height << " 1\n";
-	vtkFile << "SPACING 1.0 1.0 1.0\n";
-	vtkFile << "ORIGIN 0 0 0\n";
-	vtkFile << "POINT_DATA " << width * height << "\n";
-	vtkFile << "SCALARS u double\n";
-	vtkFile << "LOOKUP_TABLE default\n";
-
-	for (int y = 0; y < height; ++y) {
-		for (int x = 0; x < width; ++x) {
-			vtkFile << v[x][y] << " ";
-		}
-		vtkFile << "\n";
-	}
-
-	vtkFile.close();
-}
-
 
 // Function to perform one time step of the simulation
 void simulateStep() {
@@ -77,7 +42,10 @@ void simulateStep() {
 	std::vector<std::vector<double>> nextU = u;
 	std::vector<std::vector<double>> nextV = v;
 
-	double laplaceU = 0.0, laplaceV = 0.0, dU = 0.0, dV = 0.0;
+	laplaceU = 0.0;
+	laplaceV = 0.0;
+	dU = 0.0;
+	dV = 0.0;
 	double a, b;
 	for (int x = 1; x < width - 1; ++x) {
 		for (int y = 1; y < height - 1; ++y) {
@@ -131,25 +99,9 @@ TEST(size_test, u_v) {
 }
 
 TEST(answer_test, u_v_time0) {
-	u = std::vector<std::vector<double>>(width, std::vector<double>(height, 0.0));
-	v = std::vector<std::vector<double>>(width, std::vector<double>(height, 0.0));
+	init();
 	for (int iteration = 0; iteration < numIterations; ++iteration)
 		simulateStep();
-	double n = countElementsAboveThreshold(threshold);
-	ASSERT_EQ(n, 0.0);
-//	ASSERT_EQ(dU£¬Du * laplaceU);
-//	ASSERT_EQ(dV£¬Dv * laplaceV);
-
-/*	for (int i = 0; i < u.size(); ++i) {
-		for (int j = 0; j < u[i].size(); ++j) {
-			ASSERT_EQ(u[i][j], 0.0) << "u[" << i << "][" << j << "] is not zero, but it's " << u[i][j];
-		}
-	}
-
-	// Check that all elements of v are zero
-	for (int i = 0; i < v.size(); ++i) {
-		for (int j = 0; j < v[i].size(); ++j) {
-			ASSERT_EQ(v[i][j], 0.0) << "v[" << i << "][" << j << "] is not zero, but it's " << v[i][j];
-		}
-	}*/
+	ASSERT_EQ(dV, Dv * laplaceV);
+	ASSERT_EQ(dU, 1.7009796349221062e-12);
 }
